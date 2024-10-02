62f08d785d5abd1c1632d1b5a65e5dda6086e14d
Richard Ye <richardye80@yahoo.com>
2024-10-02 22:55:39 +1300

    routing issue fixed, changed fonts on a few pages

--- app/(category)/PlayerInput.tsx
+++ app/(category)/PlayerInput.tsx
@@ -16,7 +16,7 @@ const PlayerInput = () => {
         className="flex-1 w-full items-center p-5"
       >
         <View className="border-2 border-dashed bg-game_buttons_green p-5 items-center justify-center rounded-xl min-h-[300px]">
-          <Text className="text-[40px] text-white text-center">
+          <Text className="text-[40px] text-white text-center font-pangolin">
             "Think of a "CATEGORY" starting with the letter "LETTER"
           </Text>
         </View>
@@ -27,19 +27,19 @@ const PlayerInput = () => {
           </Text>
 
           <TouchableOpacity className="border-2 border-dashed bg-orange-600 p-4 w-[30%] rounded-xl items-center justify-center">
-            <Text className="text-[40px] text-white">Pass</Text>
+            <Text className="text-[40px] text-white font-pangolin">Pass</Text>
           </TouchableOpacity>
         </View>
 
         {/* Hints Section */}
         <View className="p-1 w-[60%] mt-16">
-          <Text className="text-[30px] border-2 border-dashed bg-green-700 w-full text-center rounded-xl text-white mb-5">
+          <Text className="text-[30px] border-2 border-dashed bg-green-700 w-full text-center rounded-xl text-white mb-5 font-pangolin">
             Hints
           </Text>
 
           {/* First hint */}
           <View className="flex-row justify-between m-1">
-            <Text className="text-[30px] text-white m-2">Partial Word</Text>
+            <Text className="text-[30px] text-white m-2 font-pangolin">Partial Word</Text>
             <TouchableOpacity className="border-2 border-dashed bg-green-700 p-2">
               <Text className="text-[30px] text-white">Use</Text>
             </TouchableOpacity>
@@ -47,7 +47,7 @@ const PlayerInput = () => {
 
           {/* Second hint */}
           <View className="flex-row justify-between m-1">
-            <Text className="text-[30px] text-white m-2">Full Word</Text>
+            <Text className="text-[30px] text-white m-2 font-pangolin">Full Word</Text>
             <TouchableOpacity className="border-2 border-dashed bg-green-700 p-2">
               <Text className="text-[30px] text-white">Use</Text>
             </TouchableOpacity>
@@ -55,7 +55,8 @@ const PlayerInput = () => {
 
           {/* Third hint */}
           <View className="flex-row justify-between m-1">
-            <Text className="text-[30px] text-white m-2">🔊</Text>
+            {/* Maybe replace with an image instead of emoji */}
+            <Text className="text-[30px] text-white m-2 font-pangolin">🔊</Text>
             <TouchableOpacity className="border-2 border-dashed bg-green-700 p-2">
               <Text className="text-[30px] text-white">Use</Text>
             </TouchableOpacity>
