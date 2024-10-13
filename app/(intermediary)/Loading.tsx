2fc2dc00b6a9cd70e2a4ef15a1053db6ab57994e
loops <richardye80@yahoo.com>
2024-10-14 12:10:39 +1300

    added scrollview to loading

--- app/(intermediary)/Loading.tsx
+++ app/(intermediary)/Loading.tsx
@@ -1,4 +1,11 @@
-import { Text, View, ActivityIndicator, Dimensions, ImageBackground } from "react-native";
+import {
+  Text,
+  View,
+  ActivityIndicator,
+  Dimensions,
+  ImageBackground,
+  ScrollView,
+} from "react-native";
 import React, { useEffect, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import "te-kemu-arapu-compx374-team-rauru/global.css";
@@ -8,7 +15,7 @@ import { useLanguage } from "te-kemu-arapu-compx374-team-rauru/context/languageT
 const randomFact = FACTS[Math.floor(Math.random() * FACTS.length)];
 
 const Loading = () => {
-  const { getText } = useLanguage();    
+  const { getText } = useLanguage();
 
   // This is so that the page changes size in real time when screen size changes
   const [windowDimensions, setWindowDimensions] = useState(
@@ -24,45 +31,48 @@ const Loading = () => {
 
   return (
     <SafeAreaView className="flex-1 w-full justify-center items-center bg-[#A01D1D]">
-
-       <View style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0 }}>
-        <ImageBackground
-          source={require("te-kemu-arapu-compx374-team-rauru/assets/images/tekemuarapu-bg-80.jpg")}
-          style={{
-            flex: 1,
-            width: '100%',
-            height: '100%',
-            opacity: 0.5,
-          }}
-          resizeMode="cover"
-        />
-      </View>
-
-      <View className="m-[50px] w-[80%]">
-        <Text className="font-pangolin text-[70px] text-center p-5">
-          {getText("loading")}
-        </Text>
-
-        <ActivityIndicator size={"large"} color={"black"} />
-      </View>
-
-      <View>
-        <Text
-          style={{ fontSize: windowDimensions.width < 1036 ? 50 : 70 }}
-          className="font-pangolin text-center p-5 border-2 border-dashed rounded-xl bg-green-700 mb-[30px]"
+      <ScrollView contentContainerStyle={{ alignItems: "center" }} className="pb-10">
+        <View
+          style={{ position: "absolute", top: 0, left: 0, right: 0, bottom: 0 }}
         >
-        {getText("didYouKnow")}
-        </Text>
-      </View>
+          <ImageBackground
+            source={require("te-kemu-arapu-compx374-team-rauru/assets/images/tekemuarapu-bg-80.jpg")}
+            style={{
+              flex: 1,
+              width: "100%",
+              height: "100%",
+              opacity: 0.5,
+            }}
+            resizeMode="cover"
+          />
+        </View>
 
-      <View className="w-[70%] p-1">
-        <Text
-          style={{ fontSize: windowDimensions.width < 1036 ? 40 : 60 }}
-          className="text-center font-pangolin text-[60px] border-2 border-dashed rounded-xl bg-green-700"
-        >
-          {randomFact}
-        </Text>
-      </View>
+        <View className="m-[50px] w-[80%]">
+          <Text className="font-pangolin text-[70px] text-center p-5">
+            {getText("loading")}
+          </Text>
+
+          <ActivityIndicator size={"large"} color={"black"} />
+        </View>
+
+        <View>
+          <Text
+            style={{ fontSize: windowDimensions.width < 1036 ? 50 : 70 }}
+            className="font-pangolin text-center p-5 border-2 border-dashed rounded-xl bg-green-700 mb-[30px]"
+          >
+            {getText("didYouKnow")}
+          </Text>
+        </View>
+
+        <View className="w-[70%] p-1">
+          <Text
+            style={{ fontSize: windowDimensions.width < 1036 ? 40 : 60 }}
+            className="text-center font-pangolin text-[60px] border-2 border-dashed rounded-xl bg-green-700"
+          >
+            {randomFact}
+          </Text>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
